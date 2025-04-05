5a009b1e11deb098fe297368d19f26855d85b1f5
makeshiftman <107703938+makeshiftman@users.noreply.github.com>
2025-04-05 01:15:57 +0100

    Update smartui-loader.js

--- scripts/smartui-loader.js
+++ scripts/smartui-loader.js
@@ -64,7 +64,7 @@ async function loadScenario(path) {
       const dd = String(d.getDate()).padStart(2, '0');
       const hh = String(d.getHours()).padStart(2, '0');
       const min = String(d.getMinutes()).padStart(2, '0');
-      return `${yyyy}.${mm}.${dd} ${hh}:${min}`;
+      return `${dd}.${mm}.${yyyy} ${hh}:${min}`;
     }
 
     if (data.contract_Start_Offset !== undefined) {
